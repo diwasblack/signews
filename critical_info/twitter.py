7a64a1cb46b7a6e84cc48e93a19dce656f3759d9
Diwas Sharma <diwasblack@gmail.com>
2018-02-28 09:53:14 -0600

    Code cleanup for twitter

--- critical_info/twitter.py
+++ critical_info/twitter.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import urlencode
 
 import oauth2 as oauth
 
@@ -23,3 +24,35 @@ def send_request(request_url, method="GET"):
     content = content.decode()
 
     return response, content
+
+
+def main():
+    # url = "https://api.twitter.com/1.1/search/tweets.json?q=from%3ANasa%20OR%20%23nasa"
+    # url = "https://api.twitter.com/1.1/statuses/show.json?id=962487358028484608"
+
+    # url = "https://stream.twitter.com/1.1/statuses/sample.json"
+    # url = "https://stream.twitter.com/1.1/statuses/filter.json?track=twitter"
+
+    # url = "https://api.twitter.com/1.1/trends/place.json?id=1"
+
+    url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+
+    url_parameters = {
+        "screen_name": "nypdnews",
+        "count": "3200",
+        "trim_user": "true",
+        "exclude_replies": "true",
+        "tweet_mode": "extended"
+    }
+
+    url = "{}?{}".format(url, urlencode(url_parameters))
+
+    response, content = send_request(url, "GET")
+
+    with open("response.json", "w") as file:
+        print(content)
+        file.write(content)
+
+
+if __name__ == "__main__":
+    main()

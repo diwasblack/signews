9ed3f85115791f916092fe56c95e83c79f725fc6
Diwas Sharma <diwasblack@gmail.com>
2018-02-27 22:32:54 -0600

    Keywords based critical content detection

---
+++ classifier.py
@@ -0,0 +1,31 @@
+import json
+
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+
+
+class CriticalTextDetector():
+    """
+    Class to find the critical text based on keywords
+    """
+
+    def __init__(self):
+        with open("critical_words.json", "r") as file:
+            # Construct a lookup table for keywords
+            self.keywords = set(json.load(file))
+        self.stemmer = SnowballStemmer("english")
+
+    def detect(self, text):
+        text_data = text.lower()
+        tokens = nltk.word_tokenize(text_data)
+
+        count = 0
+        for token in tokens:
+            stemmed_token = self.stemmer.stem(token)
+            if stemmed_token in self.keywords:
+                count += 1
+
+        if count >= 3:
+            return True
+        else:
+            return False

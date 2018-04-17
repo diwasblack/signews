b1bfd169792a0f146ba91760a0ac1501c216b6fb
Diwas Sharma <diwasblack@gmail.com>
2018-04-17 17:33:28 -0500

    Added helper code to compute the IDF values

---
+++ calculate_idf.py
@@ -0,0 +1,22 @@
+from crits.vectorizer import TFIDF
+
+from crits.database import Tweet
+
+
+def calculate_idf():
+    # Obtain all tweets
+    tweet_objects = Tweet.select(Tweet.body)
+    tweets = [tweet.body for tweet in tweet_objects]
+
+    tf_idf_vectorizer = TFIDF()
+    tf_idf_vectorizer.calculate_idf(tweets)
+
+    word_idf_list = tf_idf_vectorizer.get_words_idf()
+
+    with open("idf.txt", "w") as file:
+        for word, idf in word_idf_list:
+            file.write("{},{}\n".format(word, idf))
+
+
+if __name__ == "__main__":
+    calculate_idf()

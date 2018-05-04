16f6ad8837430adfb0022c164b2869084e201dc5
Diwas Sharma <diwasblack@gmail.com>
2018-05-04 15:20:58 -0500

    Added code to show misclassified tweets

--- critical_tweets.py
+++ critical_tweets.py
@@ -47,6 +47,18 @@ def train_classifier():
     logger.info(precision_recall_fscore_support(
         label_vectors, predicted_class_labels))
 
+    fn_file_path = open("false_negative.txt", "w")
+    fp_file_path = open("false_positive.txt", "w")
+
+    for index, value in enumerate(label_vectors):
+        if value == 1 and predicted_class_labels[index] == -1:
+            fn_file_path.write("{}\n\n".format(random_tweets[index][0]))
+        if value == -1 and predicted_class_labels[index] == 1:
+            fp_file_path.write("{}\n\n".format(random_tweets[index][0]))
+
+    fn_file_path.close()
+    fp_file_path.close()
+
 
 if __name__ == "__main__":
     train_classifier()

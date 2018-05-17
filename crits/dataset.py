d205e3112304609ee4f15ddaf7c6097ff8421ba4
Diwas Sharma <diwasblack@gmail.com>
2018-05-17 13:36:09 -0500

    Added method to dump/save the dataset to txt files

--- crits/dataset.py
+++ crits/dataset.py
@@ -8,11 +8,56 @@ class CriticalTextDataset():
     """
 
     def __init__(self):
-        self.dataset_path = os.path.join(
-            os.path.dirname(__file__),
-            "dataset.pkl"
-        )
+        base_path = os.path.dirname(__file__)
+        self.dataset_path = os.path.join(base_path, "dataset.pkl")
+
+        self.critical_text_path = os.path.join(
+            base_path, "dataset_critical.txt")
+        self.non_critical_text_path = os.path.join(
+            base_path, "dataset_non_critical.txt")
+
+        self.text_seperator = "\n$$end$$\n"
 
     def load_dataset(self):
         with open(self.dataset_path, "rb") as file:
             return pickle.load(file)
+
+    def dump_data(self):
+        """
+        Dump data to txt files
+        """
+
+        with open(self.dataset_path, "rb") as file:
+            texts, labels = pickle.load(file)
+
+            critical_text_file = open(self.critical_text_path, "w")
+            non_critical_text_file = open(self.non_critical_text_path, "w")
+
+            for text, label in zip(texts, labels):
+                text_with_suffix = "{}{}".format(text, self.text_seperator)
+                if(label == 1):
+                    critical_text_file.write(text_with_suffix)
+                else:
+                    non_critical_text_file.write(text_with_suffix)
+
+    def save_data(self):
+        """
+        Load the data from the txt files
+        """
+
+        with open(self.dataset_path, "wb") as file:
+
+            critical_text_file = open(self.critical_text_path, "r")
+            content = critical_text_file.read()
+            critical_texts = content.split(self.text_seperator)
+            critical_text_file.close()
+
+            non_critical_text_file = open(self.non_critical_text_path, "r")
+            content = non_critical_text_file.read()
+            non_critical_texts = content.split(self.text_seperator)
+            non_critical_text_file.close()
+
+            labels = [1] * len(critical_texts) + [0] * len(non_critical_texts)
+            dataset = critical_texts + non_critical_texts
+
+            pickle.dump((dataset, labels), file)
